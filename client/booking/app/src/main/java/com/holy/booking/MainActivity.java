39ba3346259404a5300bea4bc3707c33275812c4
HolyLuo <Hailin@Mac.local>
2018-04-18 10:11:36 +0800

    界面框架搭建--app

--- client/booking/app/src/main/java/com/holy/booking/MainActivity.java
+++ client/booking/app/src/main/java/com/holy/booking/MainActivity.java
@@ -1,14 +1,36 @@
 package com.holy.booking;
 
+import android.support.design.widget.BottomNavigationView;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.holy.common.app.Activity;
+import com.holy.common.widget.PortraitView;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
-    @BindView(R.id.txt_test)
-    TextView mText;
+
+    @BindView(R.id.appbar)
+    View mLayAppBar; 
+
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
 
     @Override
     protected int getContentLayoutID() {
@@ -19,6 +41,19 @@ public class MainActivity extends Activity {
     protected void initWidget() {
         super.initWidget();
 
-        mText.setText("hello, i am holy.");
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View, GlideDrawable>(mLayAppBar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(resource.getCurrent());
+                    }
+                });
+    }
+
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick() {
+
     }
 }
---
+++ client/booking/common/src/main/java/com/holy/common/widget/PortraitView.java
@@ -0,0 +1,27 @@
+package com.holy.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * @author Hailin
+ * @time 2018/4/17 21:38
+ * @function
+ */
+
+public class PortraitView extends CircleImageView {
+
+    public PortraitView(Context context) {
+        super(context);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
