8ad491aef969591cff40f20b9cc067117e9e8eb8
HolyLuo <Hailin@Mac.local>
2018-04-18 13:23:25 +0800

    fragment创建--app

--- client/booking/app/src/main/java/com/holy/booking/activity/MainActivity.java
+++ client/booking/app/src/main/java/com/holy/booking/activity/MainActivity.java
@@ -1,7 +1,8 @@
-package com.holy.booking;
+package com.holy.booking.activity;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -11,6 +12,11 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.ViewTarget;
+import com.holy.booking.R;
+import com.holy.booking.fragment.main.HomeFragment;
+import com.holy.booking.fragment.main.MineFragment;
+import com.holy.booking.fragment.main.ResFragment;
+import com.holy.booking.helper.NavHelper;
 import com.holy.common.app.Activity;
 import com.holy.common.widget.PortraitView;
 
@@ -18,7 +24,8 @@ import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends Activity
-        implements BottomNavigationView.OnNavigationItemSelectedListener{
+        implements BottomNavigationView.OnNavigationItemSelectedListener,
+        NavHelper.OnTabChangedListener<Integer> {
 
     @BindView(R.id.appbar)
     View mLayAppBar;
@@ -35,6 +42,8 @@ public class MainActivity extends Activity
     @BindView(R.id.navigation)
     BottomNavigationView mNavigation;
 
+    private NavHelper<Integer> mNavHelper;
+
     @Override
     protected int getContentLayoutID() {
         return R.layout.activity_main;
@@ -44,7 +53,16 @@ public class MainActivity extends Activity
     protected void initWidget() {
         super.initWidget();
 
-        mNavigation.setOnNavigationItemSelectedListener(this );
+        // 初始化底部辅助工具类
+        mNavHelper = new NavHelper<>(this, R.id.lay_container,
+                getSupportFragmentManager(), this);
+        mNavHelper.add(R.id.action_home, new NavHelper.Tab<>(HomeFragment.class, R.string.title_home))
+                .add(R.id.action_group, new NavHelper.Tab<>(ResFragment.class, R.string.title_res))
+                .add(R.id.action_contact, new NavHelper.Tab<>(MineFragment.class, R.string.title_mine));
+
+
+        // 添加对底部按钮点击的监听
+        mNavigation.setOnNavigationItemSelectedListener(this);
 
         Glide.with(this)
                 .load(R.drawable.bg_src_morning)
@@ -57,6 +75,16 @@ public class MainActivity extends Activity
                 });
     }
 
+    @Override
+    protected void initData() {
+        super.initData();
+
+        // 从底部导中接管我们的Menu，然后进行手动的触发第一次点击
+        Menu menu = mNavigation.getMenu();
+        // 触发首次选中Home
+        menu.performIdentifierAction(R.id.action_home, 0);
+    }
+
     @OnClick(R.id.im_search)
     void onSearchMenuClick() {
 
@@ -66,5 +94,11 @@ public class MainActivity extends Activity
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         mTitle.setText(item.getTitle());
         return true;
+    }
+
+    @Override
+    public void onTabChanged(NavHelper.Tab<Integer> newTab, NavHelper.Tab<Integer> oldTab) {
+        // 从额外字段中取出我们的Title资源Id
+        mTitle.setText(newTab.extra);
     }
 }
---
+++ client/booking/app/src/main/java/com/holy/booking/fragment/main/ResFragment.java
@@ -0,0 +1,15 @@
+package com.holy.booking.fragment.main;
+
+
+import com.holy.booking.R;
+import com.holy.common.app.Fragment;
+
+
+public class ResFragment extends Fragment {
+
+    @Override
+    protected int getContentLayoutID() {
+        return R.layout.fragment_res;
+    }
+
+}
