7a595a23e55457892f9ef9dfe36eb783f6e3ea01
HolyLuo <Hailin@Mac.local>
2018-05-06 16:28:14 +0800

    添加com.android.support:cardview-v7依赖

---
+++ client/booking/app/src/main/java/com/holy/booking/MyApp.java
@@ -0,0 +1,23 @@
+package com.holy.booking;
+
+import com.holy.common.app.Application;
+import com.holy.factory.Factory;
+import com.igexin.sdk.PushManager;
+
+/**
+ * @author Hailin
+ * @time 2018/4/23 19:34
+ * @function
+ */
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 调用Factory进行初始化
+        Factory.setup();
+        // 推送进行初始化
+        PushManager.getInstance().initialize(this);
+    }
+}
